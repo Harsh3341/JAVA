375e0848dc046e1f545c57472790fdf03e68a307
Harsh Gupta <101264150+Harsh3341@users.noreply.github.com>
2022-06-29 20:02:57 +0530

    Cyclic Sort

---
+++ 08-Sorting/src/DuplicateNumber.java
@@ -0,0 +1,34 @@
+public class DuplicateNumber {
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 4, 4, 3 };
+
+        System.out.println(findDuplicate(arr));
+    }
+
+    static int findDuplicate(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        // find duplicate number
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                return nums[index];
+            }
+        }
+
+        return -1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
---
+++ 08-Sorting/src/SetMismatch.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class SetMismatch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 1 };
+        findDuplicates(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static int[] findDuplicates(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        // find all duplicate numbers
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                return new int[] { nums[index], index + 1 };
+            }
+        }
+
+        return new int[] { -1, -1 };
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+}

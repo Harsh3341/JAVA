493e9aa9e552992d5ad38dabbfd1dd1bb90c054e
Harsh <harshgupta3341@gmail.com>
2022-09-25 17:43:12 +0530

    practiced merge sort

--- 11-Recursion/src/Pattern_Questions/Triangle.java
+++ 11-Recursion/src/Pattern_Questions/Triangle.java
@@ -8,7 +8,7 @@ public class Triangle {
         // triangle2(4, 0);
 
         int[] arr = { 4, 3, 2, 1 };
-        bubbleSort(arr, arr.length - 1, 0);
+        selection(arr, arr.length, 0, 0);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -58,5 +58,27 @@ public class Triangle {
         } else {
             bubbleSort(arr, r - 1, 0);
         }
+    }
+
+    static void selection(int[] arr, int r, int c, int max) {
+        if (r == 0) {
+            return;
+        }
+
+        if (c < r) {
+            if (arr[c] > arr[max]) {
+                selection(arr, r, c + 1, c);
+            } else {
+                selection(arr, r, c + 1, max);
+            }
+
+        } else {
+            int temp = arr[max];
+            arr[max] = arr[r - 1];
+            arr[r - 1] = temp;
+
+            selection(arr, r - 1, 0, 0);
+        }
+
     }
 }

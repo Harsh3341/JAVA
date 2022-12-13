db3e1a71194c67a51ea191e45a41a8067002675f
Harsh <harshgupta3341@gmail.com>
2022-12-13 19:43:32 +0530

    Time: 23 ms (38.54%), Space: 49.8 MB (53.57%)

--- LeetCode/Easy/557-Reverse-Words-in-a-String-III.java
+++ LeetCode/Easy/557-Reverse-Words-in-a-String-III.java
@@ -25,3 +25,27 @@ class Solution {
     }
 
 }
+
+// Time: 23 ms (38.54%), Space: 49.8 MB (53.57%)- this is because of the
+// StringBuilder in the reverseWords method below. The String concatenation is
+// O(n^2) while StringBuilder.append() is O(n).
+class Solution2 {
+
+    public String reverseWords(String s) {
+        StringBuilder result = new StringBuilder();
+        int lastSpaceIndex = -1;
+        for (int strIndex = 0; strIndex < s.length(); strIndex++) {
+            if ((strIndex == s.length() - 1) || s.charAt(strIndex) == ' ') {
+                int reverseStrIndex = (strIndex == s.length() - 1) ? strIndex : strIndex - 1;
+                for (; reverseStrIndex > lastSpaceIndex; reverseStrIndex--) {
+                    result.append(s.charAt(reverseStrIndex));
+                }
+                if (strIndex != s.length() - 1) {
+                    result.append(' ');
+                }
+                lastSpaceIndex = strIndex;
+            }
+        }
+        return new String(result);
+    }
+}
